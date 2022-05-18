d865481bef2bd766d5e491cad68182ccb8009c54
arthur-fellipe <arthurfellipe@gmail.com>
2022-05-18 00:00:43 -0300

    Modifica título e remove logo do header

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,15 +53,3 @@ var swiper = new Swiper(".swiper", {
   },
 
 });
-/*
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-});*/
